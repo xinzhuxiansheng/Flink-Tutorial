b1a1b42eda8185db95a121f33c34ecfb2bf379f7
xinzhuxiansheng <xinzhuxiansheng@126.com>
2021-02-21 22:18:13 +0800

    add

--- src/main/scala/com/yzhou/wc/wordcount/StreamWordCount.scala
+++ src/main/scala/com/yzhou/wc/wordcount/StreamWordCount.scala
@@ -1,15 +1,21 @@
 package com.yzhou.wc.wordcount
 
-import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, createTypeInformation}
+import org.apache.flink.streaming.api.scala._
 
+/**
+ * 脚本启动流程
+ * 1. WSL中启动 7777端口 (nc -l 7777)
+ * 2. 执行main方法，监听 WSL linux虚机 IP + Port
+ */
 object StreamWordCount {
 
   def main(args: Array[String]): Unit = {
     //创建流处理的执行环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
+    //利用nc模拟socket通信 nc -l 7777
     //接受一个socket流文本
-    val dataStream = env.socketTextStream("localhost", 7777)
+    val dataStream = env.socketTextStream("172.19.187.24", 7777)
 
     //对每条数据进行处理
     val wordCountDataStream = dataStream
@@ -19,7 +25,8 @@ object StreamWordCount {
       .keyBy(0)
       .sum(1)
 
-    wordCountDataStream.print()
+    //setParallelism 设置并行度， 会根据本地CPU核数决定
+    wordCountDataStream.print().setParallelism(1)
 
     //启动executor
     env.execute("steam word count job")
